def5fb2cfa492a26195b76207293b7af68308a08
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2022-02-16 12:17:13 +0100

    First commit

---
+++ app/src/main/java/com/alejandro/coolnotes/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,14 @@
+package com.alejandro.coolnotes.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    public DashboardViewModel() {
+
+    }
+
+
+}
