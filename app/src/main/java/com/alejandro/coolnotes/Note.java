c54c7decd541ec511628fe774394f0c91bdc5724
Alejandro Rojas Ruiz <alejandrorojasruiz7@gmail.com>
2022-02-17 11:23:59 +0100

    Update

---
+++ app/src/main/java/com/alejandro/coolnotes/Note.java
@@ -0,0 +1,25 @@
+package com.alejandro.coolnotes;
+
+public class Note {
+    private String tittle;
+    private String description;
+
+    public Note() {
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
